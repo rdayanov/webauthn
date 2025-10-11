44b18532f6353053118eda379b4070ef940aaf19
rdayanov <dayanov.radmir@gmail.com>
2025-10-11 13:21:03 +0200

    feat(cookies): make cookies httpOnly and secure

--- src/plugins/cookie.ts
+++ src/plugins/cookie.ts
@@ -2,5 +2,7 @@ import fp from 'fastify-plugin'
 import cookiePlugin from '@fastify/cookie'
 
 export default fp((fastify) => {
-  fastify.register(cookiePlugin)
+  fastify.register(cookiePlugin, {
+    parseOptions: { secure: true, httpOnly: true, sameSite: true },
+  })
 })
--- src/plugins/session.ts
+++ src/plugins/session.ts
@@ -4,5 +4,5 @@ import sessionPlugin from '@fastify/session'
 const SESSION_SECRET = process.env.SESSION_SECRET as string
 
 export default fp(async function (fastify, _opts) {
-  fastify.register(sessionPlugin, { secret: SESSION_SECRET, cookie: { secure: false } })
+  fastify.register(sessionPlugin, { secret: SESSION_SECRET })
 })
